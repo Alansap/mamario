3a9f179cbe1e2987c351653f68a59f85fa89b221
Alan <alan@syn-172-100-088-002.res.spectrum.com>
2024-04-22 20:05:32 -0600

    first commit

---
+++ conecta4.py
@@ -0,0 +1,59 @@
+def printtablero(tablero):
+    print("|", end="")
+    for i in range(1,len(tablero[0])+1):
+        print(str(i) + "|", end="")
+    print("")
+    for columna in tablero:
+        print("|", end="")
+        for columna in columna:
+            print(columna + "|", end="")
+        print("")
+    #separador
+    print("+", end="")
+    for i in range(1,len(tablero[0])+1):
+        print("-", end="+")
+    print("")
+
+def tablero(x,y):
+    tablero = []
+    for i in range(x):
+        tablero.append([])
+        for j in range(y):
+            tablero[i].append(' ')
+    return tablero
+
+def colocar_ficha(tablero, columna, turno):
+    for i in range(len(tablero[0])-1, 0, -1):
+        if tablero[i][columna-1]==' ':
+            tablero[i][columna-1] = turno
+            break
+empate = False
+jug = 1;
+
+
+
+j1 = 'O'
+bot = 'X'
+#tamaño del tablero
+x=7
+y=7
+
+tablero = tablero(x,y)
+print(tablero)
+#print(tablero(x,y))
+
+printtablero(tablero)
+
+while (True):
+    print("Va el jugador", jug)
+    col = int (input())
+    if(jug == 1):
+        colocar_ficha(tablero, col, j1)
+        jug = 2
+    else:
+        colocar_ficha(tablero, col, bot)
+        jug = 1
+    printtablero(tablero)
+
+
+            
